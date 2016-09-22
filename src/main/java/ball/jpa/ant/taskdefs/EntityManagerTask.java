d5c618b0a102c73a1ee489531d4a0c079ed3e086
Allen D. Ball <ball@hcf.dev>
2016-09-22 00:00:11 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.jpa/trunk@1995 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/jpa/ant/taskdefs/EntityManagerTask.java
@@ -0,0 +1,129 @@
+/*
+ * $Id$
+ *
+ * Copyright 2016 Allen D. Ball.  All rights reserved.
+ */
+package ball.jpa.ant.taskdefs;
+
+import ball.util.MapUtil;
+import ball.util.PropertiesImpl;
+import ball.util.ant.taskdefs.AbstractClasspathTask;
+import ball.util.ant.taskdefs.AntTask;
+import ball.util.ant.taskdefs.NotNull;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.apache.tools.ant.BuildException;
+
+/**
+ * Abstract {@link.uri http://ant.apache.org/ Ant} base
+ * {@link org.apache.tools.ant.Task} for subclass implementations that
+ * manipulate {@link EntityManagerFactory} instances.
+ *
+ * {@bean.info}
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+public abstract class EntityManagerTask extends AbstractClasspathTask {
+    protected PropertiesImpl properties = null;
+    private String persistenceUnit = null;
+    private EntityManagerFactory factory = null;
+    private EntityManager manager = null;
+
+    /**
+     * Sole constructor.
+     */
+    protected EntityManagerTask() { super(); }
+
+    @NotNull
+    public String getPersistenceUnit() { return persistenceUnit; }
+    public void setPersistenceUnit(String string) { persistenceUnit = string; }
+
+    /**
+     * Method to lazily create an {@link EntityManagerFactory}.
+     *
+     * @return  The {@link EntityManagerFactory}.
+     *
+     * @see #getPersistenceUnit()
+     * @see #properties
+     * @see Persistence#createEntityManagerFactory(String,Map)
+     */
+    protected EntityManagerFactory factory() {
+        synchronized (this) {
+            if (factory == null) {
+                ClassLoader loader =
+                    Thread.currentThread().getContextClassLoader();
+
+                try {
+                    Thread.currentThread()
+                        .setContextClassLoader(getClassLoader());
+                    factory =
+                        Persistence
+                        .createEntityManagerFactory(getPersistenceUnit(),
+                                                    properties);
+                } finally {
+                    Thread.currentThread().setContextClassLoader(loader);
+                }
+            }
+        }
+
+        return factory;
+    }
+
+    /**
+     * Method to lazily create an {@link EntityManager}.
+     *
+     * @return  The an {@link EntityManager}.
+     *
+     * @see #factory()
+     */
+    protected EntityManager manager() {
+        synchronized (this) {
+            if (manager == null) {
+                manager = factory().createEntityManager();
+            }
+        }
+
+        return manager;
+    }
+
+    @Override
+    public void init() throws BuildException {
+        super.init();
+
+        properties = new PropertiesImpl();
+        MapUtil.copy(getProject().getProperties(), properties);
+        properties.configure(this);
+    }
+
+    /**
+     * {@link.uri http://ant.apache.org/ Ant}
+     * {@link org.apache.tools.ant.Task} to create an {@link EntityManager}
+     * (often with the side-effect to update a persistence unit schema).
+     *
+     * {@bean.info}
+     */
+    @AntTask("em-create")
+    public static class Create extends EntityManagerTask {
+
+        /**
+         * Sole constructor.
+         */
+        public Create() { super(); }
+
+        @Override
+        public void execute() throws BuildException {
+            super.execute();
+
+            try {
+                manager();
+            } catch (BuildException exception) {
+                throw exception;
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+                throw new BuildException(throwable);
+            }
+        }
+    }
+}
