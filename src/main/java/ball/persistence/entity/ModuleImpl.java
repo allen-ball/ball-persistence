d4e6233608bd9f074d73d919e5015dfd4e55fa7f
Allen D. Ball <ball@hcf.dev>
2017-09-04 18:23:16 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.persistence/trunk@2375 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/persistence/entity/ModuleImpl.java
@@ -0,0 +1,36 @@
+/*
+ * $Id$
+ *
+ * Copyright 2017 Allen D. Ball.  All rights reserved.
+ */
+package ball.persistence.entity;
+
+import ball.annotation.ServiceProviderFor;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+/**
+ * {@link Module} service provider for {@link ball.persistence.entity}.
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+@ServiceProviderFor({ Module.class })
+public class ModuleImpl extends SimpleModule {
+    private static final long serialVersionUID = 3636104653602191146L;
+
+    /**
+     * Sole constructor.
+     */
+    public ModuleImpl() {
+        super(ModuleImpl.class.getPackage().getName());
+    }
+
+    @Override
+    public void setupModule(Module.SetupContext context) {
+        super.setupModule(context);
+    }
+
+    @Override
+    public String toString() { return super.toString(); }
+}
