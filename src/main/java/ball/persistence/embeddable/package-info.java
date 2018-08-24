6489ab14486256852b0daef0a09132a0e0b594c1
Allen D. Ball <ball@hcf.dev>
2018-08-24 19:45:18 +0000

    Initial writing.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball.persistence/trunk@3247 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/ball/persistence/embeddable/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * $Id$
+ *
+ * Copyright 2018 Allen D. Ball.  All rights reserved.
+ */
+/**
+ * Classes for creating JPA
+ * "{@code @}{@link javax.persistence.Embeddable}s."
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ */
+@Manifest.Section
+package ball.persistence.embeddable;
+
+import ball.annotation.Manifest;
