e083d759d15bdf1ba93f4b7d6bd5f4ce2902c634
Allen D. Ball <ball@hcf.dev>
2022-02-28 10:35:25 -0800

    Generated javadoc.

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ball.persistence"},{"l":"ball.persistence.ant"},{"l":"ball.persistence.ant.taskdefs"},{"l":"ball.persistence.embeddable"},{"l":"ball.persistence.entity"}]
